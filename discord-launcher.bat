662cfbde76991605ac45a4e36de343f803629b2d
XS <kaan22717@gmail.com>
2025-05-21 22:09:53 +0200

    discord-launcher.bat

--- discord-launcher.bat
+++ discord-launcher.bat
@@ -1,7 +1,14 @@
 @echo off
+setlocal
 title Discord Launcher
 
-echo Starte Discord...
-start "" "C:\Users\%USERNAME%\AppData\Local\Discord\Update.exe"
+set "discordPath=%LOCALAPPDATA%\Discord\Update.exe"
+
+if exist "%discordPath%" (
+    echo Starte Discord...
+    start "" "%discordPath%"
+) else (
+    echo Discord not found: %discordPath%
+)
 
 pause
