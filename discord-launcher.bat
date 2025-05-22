d3e241f4dca4e6bc350dbf5507408c6ca367e291
XS <kaan22717@gmail.com>
2025-05-22 06:16:35 +0200

    discord-launcher.bat aktualisieren

--- discord-launcher.bat
+++ discord-launcher.bat
@@ -1,14 +1,2 @@
 @echo off
-setlocal
-title Discord Launcher
-
-set "discordPath=%LOCALAPPDATA%\Discord\Update.exe"
-
-if exist "%discordPath%" (
-    echo Start Discord...
-    start "" "%discordPath%"
-) else (
-    echo Discord not found: %discordPath%
-)
-
-pause
+start https://discord.com/app
